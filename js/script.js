a920f8f2d57d6d113e3d8e0e18232055456f7ead
climo_studios <climostudios@gmail.com>
2020-04-17 12:05:22 +1200

    Added Settings

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $("#navToggle").click(function(){
     $("#navList").slideToggle();
 });
+
+$("#buttonOff").click(function(){
+    $("#buttonOff").removeClass("displayBlock").addClass("displayNone");
+    $("#buttonOn").removeClass("displayNone").addClass("displayBlock");
+});
+
+$("#buttonOn").click(function(){
+    $("#buttonOn").removeClass("displayBlock").addClass("displayNone");
+    $("#buttonOff").removeClass("displayNone").addClass("displayBlock");
+});
